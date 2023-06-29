432d9e51e9f26d195c7bc7aa020d473e0ac876b8
Masa222a <guitarstring0716@gmail.com>
2023-06-29 23:51:19 +0900

    タブレイアウトを配置するためナビゲーションを作成

--- app/src/main/java/com/hannibal/replacepraeparet/MainActivity.kt
+++ app/src/main/java/com/hannibal/replacepraeparet/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.hannibal.replacepraeparet
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.hannibal.replacepraeparet.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupBottomNavMenu()
     }
+
+    private fun setupBottomNavMenu() {
+        val host: NavHostFragment = supportFragmentManager
+            .findFragmentById(R.id.navHost) as NavHostFragment
+        val navController = host.navController
+        val bottomNav = binding.bottomNavigation
+        bottomNav.setupWithNavController(navController)
+    }
+
 }
