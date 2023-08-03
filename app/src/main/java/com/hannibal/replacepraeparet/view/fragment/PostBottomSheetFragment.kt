359b4a2fadac34abaa6b030406274de3473e0bb4
Masa222a <guitarstring0716@gmail.com>
2023-08-03 19:20:10 +0900

    レイアウトの変更、SeekBarの値を調整

--- app/src/main/java/com/hannibal/replacepraeparet/view/fragment/PostBottomSheetFragment.kt
+++ app/src/main/java/com/hannibal/replacepraeparet/view/fragment/PostBottomSheetFragment.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SeekBar
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.hannibal.replacepraeparet.R
 import com.hannibal.replacepraeparet.databinding.FragmentPostBottomSheetBinding
 
 class PostBottomSheetFragment : BottomSheetDialogFragment() {
@@ -20,6 +22,40 @@ class PostBottomSheetFragment : BottomSheetDialogFragment() {
             dismiss()
         }
 
+        setUpSeekBar()
+
         return binding.root
+    }
+
+    private fun setUpSeekBar() {
+        binding.apply {
+            alertRangeSeekBar.setOnSeekBarChangeListener(
+                object : SeekBar.OnSeekBarChangeListener {
+                    override fun onProgressChanged(
+                        seekBar: SeekBar?,
+                        progress: Int,
+                        fromUser: Boolean,
+                    ) {
+                        if (progress != 0) {
+                            val progressValue = progress.toFloat() / 20
+                            if (progressValue < 1.0) {
+                                seekBarText.text = "警戒範囲　半径：　${progress.toFloat() / 20 * 1000}m"
+                            } else {
+                                seekBarText.text = "警戒範囲　半径：　${progress.toFloat() / 20}km"
+                            }
+                        } else {
+                            seekBarText.text = getString(R.string.specific_position)
+                        }
+                    }
+
+                    override fun onStartTrackingTouch(seekBar: SeekBar?) {
+                    }
+
+                    override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                    }
+
+                }
+            )
+        }
     }
 }
