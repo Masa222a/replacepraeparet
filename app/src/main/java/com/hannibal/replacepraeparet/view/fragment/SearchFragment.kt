b46df94f9e4bfb347d745b007ada4f8f2bc2382e
Masa222a <guitarstring0716@gmail.com>
2023-08-29 12:47:32 +0900

    探す画面のレイアウトを作成

--- app/src/main/java/com/hannibal/replacepraeparet/view/fragment/SearchFragment.kt
+++ app/src/main/java/com/hannibal/replacepraeparet/view/fragment/SearchFragment.kt
@@ -5,16 +5,29 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.hannibal.replacepraeparet.R
 import com.hannibal.replacepraeparet.databinding.FragmentSearchBinding
 
-class SearchFragment : Fragment() {
+class SearchFragment : Fragment(), OnMapReadyCallback {
     private lateinit var binding: FragmentSearchBinding
+    private lateinit var mMap: GoogleMap
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View {
         binding = FragmentSearchBinding.inflate(inflater, container, false)
+
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map_fragment) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
         return binding.root
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
     }
 }
